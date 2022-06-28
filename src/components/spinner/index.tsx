54ef2a6485f5fdd8fc0b72a321dfe2c115aa4430
Akshay Gupta <akshaygupta0795@gmail.com>
2022-06-28 18:00:51 +0530

    fix spinner

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,25 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import './styles.scss';
+import React, { Component } from 'react'
+import './styles.scss'
 
-interface propType {
+interface PropTypes {
   duration: number;
 }
 
-const Spinner = ({ duration }:propType) => {
-  const [spin, setSpin] = useState<boolean>(true);
-
-  const showSpinner = (duration: number) => {
-    return new Promise<void>((resolve, reject) => {
-      setTimeout(() => {
-        setSpin(false);
-        document.body.classList.remove('no-overflow');
-        resolve();
-      }, duration);
-    })
+class Spinner extends Component<PropTypes> {
+  state = {
+    spin: true,
   }
 
-  useEffect(() => {
-    showSpinner(duration).then(() => {
+  componentDidMount() {
+    const { duration } = this.props; 
+    this.showSpinner(duration).then(() => {
       setTimeout(() => {
         let spinnerEl = document.getElementById('spinner') as HTMLElement;
         if (spinnerEl) {
@@ -27,18 +20,33 @@ const Spinner = ({ duration }:propType) => {
         }
       }, 500)
     })
-    return () => setSpin(true);
-  }, []);
+  }
 
-  return (
-    <div>
-      <div className="spinner">
-        <div className="ring"></div>
-        <div className="ring"></div>
-        <div className="dot"></div>
+  showSpinner = (duration: number) => {
+    return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+        this.setState({ spin: false })
+        document.body.classList.remove('no-overflow')
+        resolve()
+      }, duration)
+    })
+  }
+
+  render() {
+    const { spin } = this.state;
+    return (
+      <div
+        className={`spinner-container ${spin ? 'show' : ''}`}
+        id="spinner"
+      >
+        <div className="spinner">
+          <div className="ring"></div>
+          <div className="ring"></div>
+          <div className="dot"></div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Spinner;
+export default Spinner
