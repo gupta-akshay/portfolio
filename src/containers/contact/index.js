a72f195391d9faa837fb48af4dd16513b9751535
Akshay Gupta <akshaygupta0795@gmail.com>
2022-06-29 13:50:24 +0530

    update map

--- src/containers/contact/index.js
+++ src/containers/contact/index.js
@@ -121,6 +121,7 @@ class Contact extends Component {
             width="100%"
             height="100%"
             referrerPolicy="no-referrer-when-downgrade"
+            style={{ filter: 'invert(90%)' }}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58688.52668488091!2d75.79722045!3d23.16899865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39637469de00ff23%3A0x7f82abdf7899d412!2sUjjain%2C%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1656359717223!5m2!1sen!2sin"
           />
         </AnimationContainer>
