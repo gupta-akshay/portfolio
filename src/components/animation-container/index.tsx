8fc789f8dd450977654a22baaf45e9e483932087
Akshay Gupta <akshaygupta0795@gmail.com>
2022-08-03 19:12:09 +0530

    Move AnimationContainer to TS

---
+++ src/components/animation-container/index.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { useInViewport } from "react-in-viewport";
+
+import { AnimationContainerProps } from 'interfaces';
+
+import 'animate.css/animate.css';
+
+const AnimationContainer = ({ id, children, height, animation, delay }: AnimationContainerProps) => {
+  const animationContainerRef = useRef<HTMLDivElement>(null);
+  const [inViewportState, setInViewportState] = useState(false);
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const [classChanged, setClassChanged] = useState(false);
+  const { inViewport } = useInViewport(animationContainerRef);
+
+  const changeClass = () => {
+    setTimeout(() => {
+      setClassChanged(true);
+    }, delay)
+  }
+
+  useEffect(() => {
+    if (inViewportState !== inViewport && !animationComplete) {
+      setInViewportState(inViewport);
+      changeClass();
+      setAnimationComplete(true);
+    }
+  }, [inViewport]);
+
+  return (
+    <div
+      ref={animationContainerRef}
+      className={classChanged ? `animation ${animation}` : ''}
+      style={{
+        opacity: classChanged ? 1 : 0,
+        height: height || 'auto',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default AnimationContainer;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,17 @@
+import React from "react";
+
 export interface BaffleTextPropTypes {
   text: string,
   callMethodTime: number,
   revealDuration: number,
   revealDelay: number,
   parentMethod: () => void,
+}
+
+export interface AnimationContainerProps {
+  id?: string,
+  children: React.ReactNode,
+  height?: string,
+  animation: string,
+  delay: number,
 }
