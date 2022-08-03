f5ca38652c2a49be80e73da1cf02555d1c6ef13a
Akshay Gupta <akshaygupta0795@gmail.com>
2022-08-03 19:40:37 +0530

    Move Spinner to TS

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import './styles.scss';
+
+type SpinnerProps = {
+  duration: number
+};
+
+const Spinner = ({ duration }: SpinnerProps) => {
+  const [spin, setSpin] = useState(true);
+
+  useEffect(() => {
+    showSpinner(duration).then(() => {
+      setTimeout(() => {
+        const element = document.getElementById('spinner') as HTMLDivElement;
+        element.remove();
+      }, 500);
+    })
+  }, []);
+
+  const showSpinner = (duration: number) => {
+    return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+        setSpin(false);
+        document.body.classList.remove('no-overflow');
+        resolve();
+      }, duration);
+    })
+  };
+
+  return (
+    <div className={`spinner-container ${spin ? 'show' : ''}`} id='spinner'>
+      <div className='spinner'>
+        <div className='ring'></div>
+        <div className='ring'></div>
+        <div className='dot'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
